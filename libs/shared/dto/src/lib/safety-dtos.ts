6707f364198c7f3e25aa8ee938535fecbfbfd2d3
unknown <m.rutkowski0909@gmail.com>
2022-11-12 13:05:11 +0100

    dtos

---
+++ libs/shared/dto/src/lib/safety-dtos.ts
@@ -0,0 +1,31 @@
+import { SafetyState } from '@smart-home/api/safety/domain';
+
+export class SafetyDto {
+  constructor(
+    public readonly id: string,
+    public readonly homeId: string,
+    public readonly name: string,
+    public readonly type: string,
+    public readonly state: SafetyState
+  ) {}
+}
+
+export class SafetyLogDto {
+  constructor(
+    public readonly id: string,
+    public readonly safetyId: string,
+    public readonly createDate: Date,
+    public readonly state: SafetyState,
+    public readonly confirmed: boolean | null,
+    public readonly confirmedAt: Date | null,
+    public readonly confirmedBy: string | null
+  ) {}
+}
+
+export class SafetyWithLogsDto {
+  constructor(
+    public readonly id: string,
+    public readonly homeId: string,
+    public readonly logs: SafetyLogDto[]
+  ) {}
+}
