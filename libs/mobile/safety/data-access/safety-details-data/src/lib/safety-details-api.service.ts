a6ec5111677944dcc8bb77bfc97258adcfe70632
unknown <m.rutkowski0909@gmail.com>
2022-11-13 00:20:22 +0100

    details store

--- libs/mobile/safety/data-access/safety-details-data/src/lib/safety-details-api.service.ts
+++ libs/mobile/safety/data-access/safety-details-data/src/lib/safety-details-api.service.ts
@@ -1,16 +1,26 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { FromType } from '@smart-home/mobile/alarm/data-access/alarm-details-data';
 import { SafetyWithLogsDto } from '@smart-home/shared/dto';
+import { GetWithLogsQuery } from '@smart-home/shared/requests';
 import { ApiUrlUtil } from '@smart-home/shared/utils';
 import { Observable } from 'rxjs';
 
 @Injectable()
-export class SafetyDetailsApi {
+export class SafetyDetailsApiService {
   constructor(private readonly http: HttpClient) {}
 
   private readonly url = ApiUrlUtil.safetyController;
 
-  getDetails(id: string): Observable<SafetyWithLogsDto> {
-    return this.http.get<SafetyWithLogsDto>(this.url + id + '/logs');
+  getDetails(
+    id: string,
+    onlyDanger: boolean,
+    from: FromType
+  ): Observable<SafetyWithLogsDto> {
+    const query = new GetWithLogsQuery(onlyDanger, from);
+
+    return this.http.get<SafetyWithLogsDto>(this.url + id + '/logs', {
+      params: new HttpParams({ fromObject: query.toQueryParams() }),
+    });
   }
 }
